55602337c2dc4519c26ccde1cf60641d6164629d
松本夕葵 <yuukima0703@gmail.com>
2024-02-20 13:50:22 +0900

    テンプレート切り分け完了

--- iroben/src/vue/pages/thirdColorLists.vue
+++ iroben/src/vue/pages/thirdColorLists.vue
@@ -1,16 +1,42 @@
 <template>
-  <div id="app">
-    <!--VueRouterを呼び出し-->
-    <nav>
-      <router-link to="/">Home</router-link>
-      <router-link to="/product">Product</router-link>
-    </nav>
-    <router-view />
-  </div>
+  <v-ons-page>
+    <custom-toolbar></custom-toolbar>
+    <color-lists :color-lists="thirdExam"></color-lists>
+  </v-ons-page>
 </template>
 
 <script>
+import customToolbar from '../../CustomToolbar.vue';
+import top from'../pages/top.vue';
+import {thirdExam} from "../../resource/thirdExam"
+import ColorLists from "@/vue/templetes/colorLists.vue";
+
 export default {
-  name: 'App',
+  name: "thirdColorLists",
+  components: {
+    ColorLists,
+    customToolbar,
+  },
+  data() {
+    return {
+      thirdExam: thirdExam,
+      thirdTitle: "3級の問題",
+      //thirdDispatchName:"addThirdFaultItems",
+    }
+  },
+  methods: {
+    pop(){
+      this.pageStack.pop();
+    },
+    push() {
+      this.pageStack.push(top);
+    },
+  },
+  props: ['pageStack'],
+  key: 'key_page4',
 }
 </script>
+
+<style lang="scss" scoped>
+@import "./src/scss/foundation/variables";
+</style>

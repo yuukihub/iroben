3f1d7042199845bd491b4e5af37798da7f37dff1
松本夕葵 <yuukima0703@gmail.com>
2024-02-20 13:43:09 +0900

    完了

--- iroben/src/App.vue
+++ iroben/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <v-ons-navigator :page-stack="pageStack">
     <component :is="page" v-for="page in pageStack" :key="page.key" :page-stack="pageStack"></component>
+    <router-view />
   </v-ons-navigator>
 </template>
 

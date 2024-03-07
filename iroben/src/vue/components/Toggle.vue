47af8cbd00cd2b31c9aec25bb14346e93f9f8561
松本夕葵 <yuukima0703@gmail.com>
2024-03-07 12:47:38 +0900

    全色/間違えた色のtoggle切替機能

---
+++ iroben/src/vue/components/Toggle.vue
@@ -0,0 +1,111 @@
+<template>
+  <div>
+    <label class="c-toggle">
+      <input type="checkbox" @click="toggle_switch">
+      <span class="slider"></span>
+      <span class="labels" data-off="OFF" data-on="ON" ></span>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Toggle",
+  methods: {
+    toggle_switch () {
+      this.$emit('onClick');
+    },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "./src/scss/foundation/variables";
+.c-toggle {
+  position: relative;
+  display: inline-block;
+  width: 72px;
+  height: 32px;
+  border-radius: 24px;
+  cursor: pointer;
+  background-color: map_get($color, white);
+
+  input {
+    display: none;
+  }
+
+  .slider {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 24px;
+    border: 2px solid map_get($color, gray03);
+    transition: all 0.4s ease-in-out;
+
+    &::before {
+      content: "";
+      position: absolute;
+      top: 2px;
+      left: 3px;
+      width: 20px;
+      height: 20px;
+      border-radius: 24px;
+      border: 2px solid map_get($color, gray03);
+      background-color: map_get($color, white);
+      transition: all 0.4s ease-in-out;
+    }
+  }
+
+  input:checked + .slider {
+    border-color: #2196F3;
+
+    &::before {
+      border-color: #2196F3;
+      background-color: #c6e5ff;
+      transform: translateX(38px);
+    }
+  }
+
+  .labels {
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    color: map_get($color, text);
+    font-size: 12px;
+    font-family: sans-serif;
+    transition: all 0.4s ease-in-out;
+
+    &::after {
+      content: attr(data-off);
+      position: absolute;
+      top: 7px;
+      right: 12px;
+      opacity: 1;
+      transition: all 0.4s ease-in-out;
+    }
+
+    &::before {
+      content: attr(data-on);
+      position: absolute;
+      top: 7px;
+      left: 16px;
+      opacity: 0;
+      transition: all 0.4s ease-in-out;
+    }
+  }
+
+  input:checked ~ .labels {
+    &::after {
+      opacity: 0;
+    }
+
+    &::before {
+      opacity: 1;
+    }
+  }
+}
+
+</style>
--- iroben/src/vue/pages/ColorPaint.vue
+++ iroben/src/vue/pages/ColorPaint.vue
@@ -1,43 +1,94 @@
 <template>
   <v-ons-page>
     <custom-toolbar></custom-toolbar>
-    <ul class="c-colorLists">
-      <li v-for="(item, index) in faultCountArray" :key="index">
-        <div class="color" v-bind:style="{background: item.colorCode}">
-          {{faultCountArray[index].title}}
-        </div>
-      </li>
-    </ul>
+
+    <div class="toggle_wrap">
+      <toggle @onClick="toggle()"></toggle>
+    </div>
+
+    <div v-if="isActive === false">
+      初期の内容
+      <div class="c-colorLists">
+        <ul>
+          <li v-for="(item, index) in secondExam" :key="index">
+            <div class="color" v-bind:style="{background: item.colorCode}">
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div v-else>
+      切り替えた内容
+      <div class="c-colorLists">
+        <ul>
+          <li v-for="(item, index) in faultCountArray" :key="index">
+            <div class="color" v-bind:style="{background: item.colorCode}">
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
   </v-ons-page>
 </template>
 
 <script>
 import customToolbar from "../components/CustomToolbar.vue";
+import {secondExam} from "@/resource/secondExam";
+import Toggle from "@/vue/components/Toggle.vue";
+
 export default {
   name: "ColorPaint",
   components: {
+    Toggle,
     customToolbar,
   },
   data(){
     return {
       faultItem: this.$store.state["second"].faultArray,
       faultCountArray: "",
+      secondExam: secondExam,
+      isActive: false
     }
   },
   created() {
     //this.$store.stateのfaultArrayに重複して入っている色は削除
     let faultItemArray = JSON.parse(JSON.stringify(this.faultItem));
-    let uniqueFaultItem = Array.from(
+    this.faultCountArray = Array.from(
         new Map(faultItemArray.map((faultItemArray) => [faultItemArray.id, faultItemArray])).values()
     );
-    this.faultCountArray = uniqueFaultItem;
+  },
+  methods: {
+    toggle() {
+      this.isActive = !this.isActive
+    }
   },
 }
 </script>
 
 <style lang="scss" scoped>
+.toggle_wrap {
+  display: flex;
+  align-items: center;
+}
+.toggle_title {
+  margin-right: 4px;
+}
+.c-colorPallet {
+  width: 100%;
+}
+.c-colorLists{
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+  }
+}
 .color {
-  width: 8vh;
-  height: 6.5vh;
+  width: 2vh;
+  height: 2vh;
+  border-radius: 100%;
+  margin: 0 4px 4px 0;
+}
+.active {
+  color:red;
 }
 </style>
